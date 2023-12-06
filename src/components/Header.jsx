f6b3991f20c31c62203df99c909f99c055d02961
nikhilDkhot <nikhillkhot369@gmail.com>
2023-12-07 01:43:41 +0530

    comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Alert, Container } from "react-bootstrap";
 
 export function Header(props){
-    return (
+    return (//omkar nikhil project
         <Container className="mt-5">
             <Alert variant="success">
                 <h6>{props.text}</h6>
